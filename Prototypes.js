47c15f1c7e30e9f3ba591d8d10991d88a6b25ab3
angeleeshaw <59425777+angeleefshaw@users.noreply.github.com>
2021-11-08 20:09:32 -0500

    Remove duplicates from array
    Throwing random code in now without much reasoning or structure

--- Prototypes.js
+++ Prototypes.js
@@ -14,10 +14,17 @@ User.prototype.logout = function() {
     this.online = false;
     console.log(this.firstName + '' + this.lastName, 'has logged out');
 }
-
+ 
 function Admin(...params) {
     User.apply(this, params)
     this.role = 'Super admin'
+}
+
+Admin.prototype.deleteUser = function(userToDelete) {
+    users = users.filter(user => {
+        return user.email !=userToDelete.email;
+    })
+
 }
 
 //admin is inheriting the Users prototype
@@ -31,3 +38,17 @@ firstUser.login();
 
 console.log(firstUser);
 console.log(admin)
+
+const arrayOfNumbers = [1,4,7,7,6,4,5,12,88,67,67,5,5,90,101,122,222,565,55,66,23,23,24,25,22,3,4,1,90,88,12,12];
+
+function removeDuplicates(arr) {
+
+    let newArr =  arr.filter((item, index) => {
+        return arr.indexOf(item) == index;
+    });
+
+    return newArr
+}
+
+console.log(removeDuplicates(arrayOfNumbers));
+
