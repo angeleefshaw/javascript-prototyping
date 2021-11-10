63bfe48654d42bffafcabea82fcf77ef917aff8c
angeleeshaw <59425777+angeleefshaw@users.noreply.github.com>
2021-11-09 23:14:36 -0500

    Structuring

--- objectPlay.js
+++ objectPlay.js
@@ -22,5 +22,14 @@ class Country {
 
 }
 
-const unitedStates = new Country('United States', 'Red, White, Blue', 331,002,651, 0.042, 'North America')
-console.log(unitedStates)
+const unitedStates = new Country('United States', 'Red, White, Blue', 331002651, 0.042, 'North America');
+const australia = new Country('Australia', 'Green, Gold', 25499884, 0.03, 'Australia/Oceania');
+
+const countryData = {
+    'UnitedStates': unitedStates,
+    'Australia': australia
+}
+
+console.log(countryData)
+
+console.log(unitedStates, australia)
