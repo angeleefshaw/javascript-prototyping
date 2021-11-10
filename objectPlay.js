2edf2450e30c0f41be3b06efcec9b533ce9b0301
angeleeshaw <59425777+angeleefshaw@users.noreply.github.com>
2021-11-09 22:24:28 -0500

    Playing with classes and objects

---
+++ objectPlay.js
@@ -0,0 +1,23 @@
+class Country {
+
+    constructor(name, nationalColors, population, worldShare, region) {
+        this.name = name;
+        this.nationalColors = this.nationalColors;
+        this.population = this.population;
+        this.worldShare = this.worldShare;
+        this.region = this.region;
+    }
+
+    orderByPopulation() {
+
+    }
+
+    getCountriesByColor() {
+
+    }
+
+    projectPopulation() {
+
+    }
+
+}
