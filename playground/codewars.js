069217edb9123a5c2c17206c80b9010f073163eb
angeleeshaw <59425777+angeleefshaw@users.noreply.github.com>
2021-12-15 21:12:06 -0500

    Create codewars.js

---
+++ playground/codewars.js
@@ -0,0 +1,7 @@
+function spinWords(words){
+    return words.split(' ').map(function (word) {
+      return (word.length > 4) ? word.split('').reverse().join('') : word;
+    }).join(' ');
+  }
+
+  spinWords("Testing my Code to see which words are reversed.")
