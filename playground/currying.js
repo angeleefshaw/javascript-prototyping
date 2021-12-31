eccb2718f7cef64571da152e93add85ba1a5bf67
angeleeshaw <59425777+angeleefshaw@users.noreply.github.com>
2021-12-31 14:06:06 -0500

    Filter Array w/currying
    Used currying to filter an array. Function is readable and modular making it easier to use and debug

--- playground/currying.js
+++ playground/currying.js
@@ -11,4 +11,32 @@ function makeGreeting(string) {
     
 let hello = makeGreeting('Spicy');
 let helloWorld = hello('Pineapple');
-console.log(helloWorld);
+//console.log(helloWorld);
+
+//rewritted in arrow syntax 
+let curryOrder = string => string2 => `I would like ${string} ${string2} curry.`
+
+const employees = [
+    { age: 50, role: "team lead", department: "green", dateJoined: new Date('2021-01-20') },
+    { age: 26, role: "dev", department: "green", dateJoined: new Date('2019-12-30') },
+    { age: 34, role: "dev", department: "blue", dateJoined: new Date('2020-11-12') },
+    { age: 34, role: "dev", department: "green", dateJoined: new Date('2020-08-21') },
+    { age: 20, role: "intern", department: "green", dateJoined: new Date('2021-07-06') },
+    { age: 53, role: "team lead", department: "red", dateJoined: new Date('2019-02-27') },
+    { age: 44, role: "dev", department: "blue", dateJoined: new Date('2019-05-31') },
+    { age: 45, role: "team lead", department: "blue", dateJoined: new Date('2018-03-10') }
+]
+
+//Task - filter employees by role/age/department 
+
+// Since we’re handling each argument one off in single argument functions, 
+// we can handle validating each argument as we go through the chain and hone 
+// in on any argument errors before code proceeds or has a chance to return an empty array.
+
+const setFilter = array => key => value => array.filter(x => x[key] === value);
+
+const filterEmployees = setFilter(employees);
+const filterEmployeesByRole = filterEmployees('role');
+const filteredEmployeesByDev = filterEmployeesByRole('dev');
+ 
+console.log(filteredEmployeesByDev); // Returns an array of devs 
